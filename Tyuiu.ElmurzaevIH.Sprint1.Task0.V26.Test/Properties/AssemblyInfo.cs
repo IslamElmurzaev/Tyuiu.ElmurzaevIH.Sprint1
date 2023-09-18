aad9abe7f0c9509fc8fdaf63fbc03529db28b768
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-09-18 13:12:36 +0500

    Task0

---
+++ Tyuiu.ElmurzaevIH.Sprint1.Task0.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElmurzaevIH.Sprint1.Task0.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.ElmurzaevIH.Sprint1.Task0.V26.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3c15cd6-0692-41ab-a2a8-0479985696b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
