cdc28744c1a6b2fe46cdced52e3e18c68b06271e
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-09-20 21:26:10 +0500

    Task1

---
+++ Tyuiu.ElmurzaevIH.Sprint1.Task1.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElmurzaevIH.Sprint1.Task1.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.ElmurzaevIH.Sprint1.Task1.V27.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8df636dc-e3db-46d1-a854-5dbc7ca1e30b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
