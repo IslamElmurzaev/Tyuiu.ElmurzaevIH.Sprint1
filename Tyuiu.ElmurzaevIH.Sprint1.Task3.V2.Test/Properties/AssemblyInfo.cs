5a70717dacbee61e1bbcf02b43acf1090a17717a
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-09-22 19:14:41 +0500

    Task3

---
+++ Tyuiu.ElmurzaevIH.Sprint1.Task3.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElmurzaevIH.Sprint1.Task3.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.ElmurzaevIH.Sprint1.Task3.V2.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7232772c-4473-451b-85c2-f6d4700b9873")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ElmurzaevIH.Sprint1.Task3.V2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ElmurzaevIH.Sprint1.Task3.V2.Lib;
+
+namespace Tyuiu.ElmurzaevIH.Sprint1.Task3.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Ельмурзаев И.Х. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Ельмурзаев Ислам Хаважевич | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Объявите необходимые переменные и напишите программу вычисления         *");
+            Console.WriteLine("* стоимости покупки, состоящей из нескольких тетрадей и карандашей.       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double priceNotebook = 45; double pricePencil = 25;
+            Console.WriteLine("Цена тетради:" + priceNotebook);
+            Console.WriteLine("Цена карандаша:" + pricePencil);
+
+            int amountNotebook = 7; int amountPencil = 6;
+            Console.WriteLine("Кол-во тетрадей:" + amountNotebook);
+            Console.WriteLine("Кол-во карандашей:" + amountPencil);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Общая стоимость покупки:" + ds.PurchaseAmount(priceNotebook, amountNotebook, pricePencil, amountPencil));
+            Console.ReadKey();
+        }
+    }
+}
